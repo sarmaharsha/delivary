64a291c3b774bdaab2a1f62863f02502a5f378a9
sarmaharsha <harshasarma123@gmail.com>
2024-04-17 09:54:09 +0530

    new files added

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+let heading=React.createElement("h1",{},"Hello my dear students");
+let root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading,root);
+
